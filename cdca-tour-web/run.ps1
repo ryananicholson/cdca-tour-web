bc66718a2c15399276fdaf65c1a3d5bc4ced8f98
ryananicholson <ryananicholson@gmail.com>
2022-12-21 09:05:45 -0500

    Create run.ps1

---
+++ cdca-tour-web/run.ps1
@@ -0,0 +1,20 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+Connect-AzAccount -Identity
+Import-Module CosmosDB -UseWindowsPowerShell
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+$cosmosContext = New-CosmosDbContext -Account cdca-cosmos -Database CDCADB -ResourceGroup cdca-rg
+$results = Get-CosmosDbDocument -Context $cosmosContext -CollectionId cdca-tour-schedule
+$body = "test"
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+})
